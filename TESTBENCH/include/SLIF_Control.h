b5bf971f07c273cb2cf18481ff00a6f0a95eaed1
brenton-james <Jasper.Assembly@icloud.com>
2026-09-24 18:55:04 +1000

    DEVELOPMENT_2026_09_24_1854, <COMPILE_SUCCESS> <RUNTIME_DEV>. BUILT:   - slif::MutexQue.   - slif::LaunchQue. TODO:   - slif:ThreadsLog.
    IMPLEMENTED:
    PACKAGE SLIF.
     -
     - class SLIF_Framework.
     - class SLIF_Framework_Execute.
     - class SLIF_Framework_Global.
     CLIB MutexQue.
      - class SLIF_MutexQue.
      - class SLIF_MutexQue_Framework.
      - class SLIF_MutexQue_Framework_Global.
      - class SLIF_MutexQue_Framework_App.
      - class SLIF_MutexQue_Framework_App_Control.
     CLIB LaunchQue.
      - class
    
    <<< TERMINAL OUT <<<
    brenton-james@brenton-james-PRO-ADL-U-Cubi-5-MS-B0A8:~/CLionProjects/slif$ cmake -B build
    -- The CXX compiler identification is GNU 15.2.0
    -- Detecting CXX compiler ABI info
    -- Detecting CXX compiler ABI info - done
    -- Check for working CXX compiler: /usr/bin/c++ - skipped
    -- Detecting CXX compile features
    -- Detecting CXX compile features - done
    -- Configuring done (0.3s)
    -- Generating done (0.0s)
    -- Build files have been written to: /home/brenton-james/CLionProjects/slif/build
    brenton-james@brenton-james-PRO-ADL-U-Cubi-5-MS-B0A8:~/CLionProjects/slif$ cmake --build build
    [  9%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_MutexQue/src/SLIF_MutexQue.cpp.o
    [ 18%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_MutexQue/src/SLIF_MutexQue_Framework_App.cpp.o
    [ 27%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_MutexQue/src/SLIF_MutexQue_Framework_Global.cpp.o
    [ 36%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_MutexQue/src/SLIF_MutexQue_Framework_App_Control.cpp.o
    [ 45%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue.cpp.o
    [ 54%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework.cpp.o
    [ 63%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_Global.cpp.o
    [ 72%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_App.cpp.o
    [ 81%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_App_Control.cpp.o
    /home/brenton-james/CLionProjects/slif/Control/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_App_Control.cpp: In member function ‘void slif::SLIF_LaunchQue_Framework_App_Control::dyn_App_FUNCT_SLIF_LaunchQue_Framework_App_Control_launchQue_Update(uint8_t*, slif::SLIF_LaunchQue_Framework*, uint8_t*)’:
    /home/brenton-james/CLionProjects/slif/Control/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_App_Control.cpp:55:20: warning: switch condition has boolean value [-Wswitch-bool]
       55 |             switch (obj->dyn_CLASS_get_ptr_SLIF_LaunchQue_Framework_App(threadId)->dyn_CLASS_get_SLIF_LaunchQue_Framework_App_Control(threadId)->dyn_REG_get_SLIF_LaunchQue_Item_On_list_Of_STATE_For_ConcurrentCore(threadId, index)) {
          |                    ^
    [ 90%] Building CXX object Control/CMakeFiles/slif_control.dir/libs/SLIF_LaunchQue/src/SLIF_LaunchQue_Framework_Execute.cpp.o
    [100%] Linking CXX static library libslif_control.a
    [100%] Built target slif_control
    <<< TERMINAL END.
    
    <<< CONSOLE OUT <<<
    
    <<< CONSOLE END.

---
+++ TESTBENCH/include/SLIF_Control.h
@@ -0,0 +1,14 @@
+//
+// Created by brenton-james on 9/24/26.
+//
+
+#ifndef SILICONLIFE_SLIF_CONTROL_H
+#define SILICONLIFE_SLIF_CONTROL_H
+
+
+class SLIF_Control
+{
+};
+
+
+#endif //SILICONLIFE_SLIF_CONTROL_H
---
+++ TESTBENCH/src/SLIF_Control.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brenton-james on 9/24/26.
+//
+
+#include "../include/SLIF_Control.h"
